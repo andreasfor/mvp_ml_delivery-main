ec5160279b72265b9bc4e633c9e56df3da110e1f
anforsbe <andreas.bengt.forsberg@gmail.com>
2023-06-21 13:43:20 +0200

    Updaed functions with Sphinx docstrings

--- common_dir/common.py
+++ common_dir/common.py
@@ -1,9 +1,18 @@
 import pyspark.sql as S
 
 class Common:
+    """
+    The purpose of the Common class is to facilitate hygenic functions that is generall for the project to work. 
+    """
 
     @staticmethod
-    def create_spark_session():
+    def create_spark_session() -> S.SparkSession:
+        """
+        Creates a spark session needed when using repos and not workspace. 
+        
+        :returns: Spark session
+        :rtype: pyspark.sql.SparkSession
+        """
         spark = (S.SparkSession
                  .builder
                  .appName("FetchingSpark")
